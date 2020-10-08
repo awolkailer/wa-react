e675721175ce79a2a9ac0a032186338749d2630a
awolkailer <kaila.d.turpin@gmail.com>
2020-10-07 20:18:29 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 export default function App(){
   return (
     <div className="App">
+      <div className="container"></div>
       <h1>Weather App </h1>
 
       <footer>
@@ -16,7 +17,7 @@ export default function App(){
         </a>
         </footer>
        
-       
+       </div>
     </div>
   );
 }
