e66d601fa37edccc91e5b9e24d68024a101181ee
awolkailer <kaila.d.turpin@gmail.com>
2020-10-12 14:00:33 -0600

    added forecast

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,7 @@ import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 import Loader from "react-loader-spinner";
 import axios from "axios";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
     const [ready, setReady] = useState(false);
@@ -67,7 +68,8 @@ export default function Weather(props) {
                     </div>
                 
                 </form>
-                <WeatherInfo data={weatherData}/>
+                <WeatherInfo data={weatherData} />
+                <WeatherForecast city={weatherData.city} />
 
                 
             </div>
---
+++ src/WeatherForecast.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import axios from "axios";
+import "./WeatherForecast.css";
+import WeatherForecastPreview from"./WeatherForecastPreview";
+
+export default function WeatherForecast(props) {
+    const [loaded, setLoaded] = useState(false);
+    const [forecast, setForecast] = useState(null);
+
+    function handleForecastReponse(response) {
+        setForecast(response.data);
+        setLoaded(true);
+        
+
+    }
+
+    if (loaded && props.city === forecast.city.name) {
+        return (
+            <div className="WeatherForecast row">
+                
+        <WeatherForecastPreview data={forecast.list[0]} />
+        <WeatherForecastPreview data={forecast.list[1]} />
+        <WeatherForecastPreview data={forecast.list[2]} />
+        <WeatherForecastPreview data={forecast.list[3]} />
+        <WeatherForecastPreview data={forecast.list[4]} />
+        <WeatherForecastPreview data={forecast.list[5]} />
+                </div>
+        );
+
+    } else {
+    
+    let apiKey = "8f64ba8aed726b6e04d4af5e8025ebf4";
+    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(handleForecastReponse);
+
+    return null;
+    }
+}
